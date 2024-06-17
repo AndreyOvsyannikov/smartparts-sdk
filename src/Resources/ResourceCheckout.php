e13b7c30adcd90cc9c52c95f92e06bc07531f3e8
Kornev Denis <seenetx@gmail.com>
2024-06-17 11:11:34 +0700

    checkout/isorderable

--- src/Resources/ResourceCheckout.php
+++ src/Resources/ResourceCheckout.php
@@ -4,19 +4,11 @@ namespace Smartparts\Resources;
 
 class ResourceCheckout extends Resource
 {
-    public function postPreorder(array $data) {
+    public function storePreorder(array $data) {
         return $this->api->call('POST', 'checkout/preorder', $data);
     }
 
-    public function getNode(string $slug) {
-        return $this->api->call('GET', 'catalog/node', [
-            'slug'  => $slug,
-        ]);
-    }
-
-    public function getSpare(string $slug) {
-        return $this->api->call('GET', 'catalog/spare', [
-            'slug'  => $slug,
-        ]);
+    public function isOrderable(array $data) {
+        return $this->api->call('POST', 'checkout/is-orderable', $data);
     }
 }
